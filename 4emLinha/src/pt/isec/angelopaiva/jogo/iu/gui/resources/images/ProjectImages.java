bbf4734a894b35a66dc955878c78e26f20ca1797
RektByDead <rlousada123456@gmail.com>
2025-05-24 16:04:48 +0100

    remove gui stuff

--- 4emLinha/src/pt/isec/angelopaiva/jogo/iu/gui/resources/images/ProjectImages.java
+++
@@ -1,8 +0,0 @@
-package pt.isec.angelopaiva.jogo.iu.gui.resources.images;
-
-public class ProjectImages {
-    public static final String ICON = "icon.png";
-    public static final String TROPHY = "trophy.png";
-
-    private ProjectImages() {}
-}
